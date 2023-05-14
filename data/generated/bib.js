14e8832505a4da671c93315bf1b20fa6d068eb75
geeeer <95530027+geeeer@users.noreply.github.com>
2023-05-14 17:09:59 +0100

    Add files via upload

---
+++ data/generated/bib.js
@@ -0,0 +1,93 @@
+﻿const generatedBibEntries = {
+    "bechar2016agricultural": {
+        "author": "Bechar, Avital and Vigneault, Cl{\\'e}ment",
+        "journal": "Biosystems Engineering",
+        "pages": "94--111",
+        "publisher": "Elsevier",
+        "title": "Agricultural robots for field operations: Concepts and components",
+        "type": "article",
+        "volume": "149",
+        "year": "2016"
+    },
+    "bochkovskiy2020yolov4": {
+        "author": "Bochkovskiy, Alexey and Wang, Chien-Yao and Liao, Hong-Yuan Mark",
+        "journal": "arXiv preprint arXiv:2004.10934",
+        "title": "Yolov4: Optimal speed and accuracy of object detection",
+        "type": "article",
+        "year": "2020"
+    },
+    "chen2020using": {
+        "author": "Chen, Junde and Chen, Jinxiu and Zhang, Defu and Sun, Yuandong and Nanehkaran, Yaser Ahangari",
+        "journal": "Computers and Electronics in Agriculture",
+        "pages": "105393",
+        "publisher": "Elsevier",
+        "title": "Using deep transfer learning for image-based plant disease identification",
+        "type": "article",
+        "volume": "173",
+        "year": "2020"
+    },
+    "duan2019centernet": {
+        "author": "Duan, Kaiwen and Bai, Song and Xie, Lingxi and Qi, Honggang and Huang, Qingming and Tian, Qi",
+        "booktitle": "Proceedings of the IEEE/CVF international conference on computer vision",
+        "pages": "6569--6578",
+        "title": "Centernet: Keypoint triplets for object detection",
+        "type": "inproceedings",
+        "year": "2019"
+    },
+    "girshick2015fast": {
+        "author": "Girshick, Ross",
+        "booktitle": "Proceedings of the IEEE international conference on computer vision",
+        "pages": "1440--1448",
+        "title": "Fast r-cnn",
+        "type": "inproceedings",
+        "year": "2015"
+    },
+    "liu2020tomato": {
+        "author": "Liu, Jun and Wang, Xuewei",
+        "journal": "Frontiers in plant science",
+        "pages": "898",
+        "publisher": "Frontiers Media SA",
+        "title": "Tomato diseases and pests detection based on improved Yolo V3 convolutional neural network",
+        "type": "article",
+        "volume": "11",
+        "year": "2020"
+    },
+    "mohanty2016using": {
+        "author": "Mohanty, Sharada P and Hughes, David P and Salath{\\'e}, Marcel",
+        "journal": "Frontiers in plant science",
+        "pages": "1419",
+        "publisher": "Frontiers Media SA",
+        "title": "Using deep learning for image-based plant disease detection",
+        "type": "article",
+        "volume": "7",
+        "year": "2016"
+    },
+    "wang2018segmentation": {
+        "author": "Wang, Zhen and Zhang, Shanwen and others",
+        "journal": "Acad J Comput Inf Sci",
+        "pages": "9--18",
+        "title": "Segmentation of corn leaf disease based on fully convolution neural network",
+        "type": "article",
+        "volume": "1",
+        "year": "2018"
+    },
+    "zhang2021high": {
+        "author": "Zhang, Yan and Wa, Shiyun and Liu, Yutong and Zhou, Xiaoya and Sun, Pengshuo and Ma, Qin",
+        "journal": "Remote Sensing",
+        "number": "21",
+        "pages": "4218",
+        "publisher": "MDPI",
+        "title": "High-accuracy detection of maize leaf diseases CNN based on multi-pathway activation function module",
+        "type": "article",
+        "volume": "13",
+        "year": "2021"
+    },
+    "zhu2019feature": {
+        "author": "Zhu, Chenchen and He, Yihui and Savvides, Marios",
+        "booktitle": "Proceedings of the IEEE/CVF conference on computer vision and pattern recognition",
+        "pages": "840--849",
+        "title": "Feature selective anchor-free module for single-shot object detection",
+        "type": "inproceedings",
+        "year": "2019"
+    }
+};
